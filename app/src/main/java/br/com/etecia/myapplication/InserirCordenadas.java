2c81c1dff717e06f1aedc2c2d17f0dff8e30afe4
menezessn <mar_chelo10@hotmail.com>
2021-08-10 20:02:23 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/etecia/myapplication/InserirCordenadas.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InserirCordenadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inserir_cordenadas_layout);
+    }
+}
